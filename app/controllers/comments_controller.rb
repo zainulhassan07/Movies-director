8d42616268a0d36b88e9e84a4d735cdbc3927b82
Zain <zainhassan910@gmail.com>
2020-12-16 11:09:59 +0500

    movies-comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
+  before_action:find_movie, only: [:create]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.save
-    redirect_to movies_path
+    
+    redirect_to movie_path(@movie)
   end
 
   private
     def comment_params
       params.require(:comment).permit(:body, :movie_id)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:comment][:movie_id])
     end
 end
