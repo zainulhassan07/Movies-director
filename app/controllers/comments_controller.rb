13d17b7f3ce66e503d9f0c38097b38cf45fcce59
Zain <zainhassan910@gmail.com>
2020-12-16 14:56:27 +0500

    movies-delete-comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
   before_action:find_movie, only: [:create]
+  before_action:find_movieid_ondestroy, only: [:destroy]
+
+  
 
   def create
     @comment = Comment.new(comment_params)
     @comment.save
     
     redirect_to movie_path(@movie)
+  end
+
+
+  def destroy
+    @comment = Comment.find(params[:id]).destroy
+    @comment.destroy
+    redirect_to movie_path(@movie)	
   end
 
   private
@@ -16,4 +26,9 @@ class CommentsController < ApplicationController
     def find_movie
       @movie = Movie.find(params[:comment][:movie_id])
     end
+
+    def find_movieid_ondestroy
+      @movie = Movie.find(params[:movie_id])
+    end
+
 end
